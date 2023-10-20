f39220fe9658801b3fb0b3709d0688d4f85c9d06
AlexKriv <krivouzis1985@gmail.com>
2023-10-20 12:02:25 +0300

    linked match rendering to storage

--- teams.js
+++ teams.js
@@ -1,5 +1,5 @@
 const source = {
-  groups: ["a", "b", "c"],
+  groups: ["a", "b"],
   teams: [
     {
       id: 1,
@@ -61,6 +61,42 @@ const source = {
       goals: { for: 0, against: 0 },
       owner: "",
     },
+    {
+      id: 6,
+      group: "b",
+      country: "Ukraine",
+      name: "Shakhtar Donetsk",
+      played: 0,
+      win: 0,
+      draw: 0,
+      lose: 0,
+      goals: { for: 0, against: 0 },
+      owner: "",
+    },
+    {
+      id: 7,
+      group: "b",
+      country: "Ukraine",
+      name: "Shakhtar Donetsk",
+      played: 0,
+      win: 0,
+      draw: 0,
+      lose: 0,
+      goals: { for: 0, against: 0 },
+      owner: "",
+    },
+    {
+      id: 8,
+      group: "b",
+      country: "Ukraine",
+      name: "Shakhtar Donetsk",
+      played: 0,
+      win: 0,
+      draw: 0,
+      lose: 0,
+      goals: { for: 0, against: 0 },
+      owner: "",
+    },
   ],
 };
 
@@ -92,16 +128,15 @@ if (localStorage.length === 0) {
   data.groups.forEach((gr) => {
     renderGroup(gr);
     const rounds = roundRobin(filteredTeamsNameByGroup(gr));
-    console.log(rounds);
-    //  rounds.length === 6 ? save(`matches-${gr}`, rounds) : [];
-    renderMatches(rounds, gr);
+    const matches = load(`matches-${gr}`);
+    !matches && save(`matches-${gr}`, rounds);
+    renderMatches(gr);
   });
 }
 
 function filteredTeamsNameByGroup(group) {
   return data.teams.filter((t) => t.group === group).map((t) => t.name);
 }
-
 function renderTable() {
   const markup = data.groups
     .map((gr) => {
@@ -187,24 +222,70 @@ function roundRobin(teams) {
 
   return matches;
 }
-function renderMatches(teams, group) {
-  const markup = teams.map((t) => {
-    return `<li class="match" id=""><p><span data-team="home">${t[0]}</span>  <span class="home">0</span> - <span class="away">0</span>  <span data-team="away">${t[1]}</span></p><div class="modal">
-  <div class="modal-content">
-       <form>
-      <input type="number" name="home" placeholder="${t[0]}">
-      <span> - </span>
-      <input type="number" name="away" placeholder="${t[1]}" />
-      <button type="submit">OK</button>
-		</form>	
+function renderMatches(group) {
+  const matches = load(`matches-${group}`);
+
+  const markup = matches.map((t) => {
+    return `<li class="match" data-group=${group}><p><span data-team="home">${t.match[0]}</span>  <span class="home">${t.score[0]}</span> - <span class="away">${t.score[1]}</span>  <span data-team="away">${t.match[1]}</span></p><div class="modal">
+    <div class="modal-content">
+         <form>
+        <input type="number" name="home" placeholder="${t.match[0]}">
+        <span> - </span>
+        <input type="number" name="away" placeholder="${t.match[1]}"/>
+        <button type="submit">OK</button>
+  		</form>
+    </div>
   </div>
-</div>
-</li>`;
+  </li>`;
   });
-  document
-    .querySelector(`ul[data-list='${group}']`)
-    .insertAdjacentHTML("beforeend", markup.join(""));
+  document.querySelector(`ul[data-list='${group}']`).innerHTML =
+    markup.join("");
+  const list = document.querySelectorAll("li");
+  list.forEach((el) => {
+    el.addEventListener("click", modalOpen);
+  });
 }
 function getID() {
   return "id" + Math.random().toString(16).slice(2);
+}
+//!=========== Список матчей с модалкой =================
+
+function modalOpen(e) {
+  const modal = e.target.closest("li").querySelector(".modal");
+  const form = e.target.closest("li").querySelector("form");
+  const closeBtn = form.querySelector("button");
+
+  modal.classList.add("open");
+
+  form.addEventListener("submit", handleSubmit);
+  closeBtn.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  const modal = document.querySelector(".open");
+  modal.classList.replace("open", "close");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const teamHome = form.closest("li").querySelector("span[data-team='home']");
+  const teamAway = form.closest("li").querySelector("span[data-team='away']");
+  const group = form.closest("li").dataset.group;
+  const matches = load(`matches-${group}`);
+  const home = form.elements.home.value;
+  const away = form.elements.away.value;
+
+  const match = {
+    match: [teamHome.textContent, teamAway.textContent],
+    score: [Number(home) || 0, Number(away) || 0],
+  };
+  const index = matches.findIndex(
+    (m) =>
+      m.match[0] + m.match[1] === teamHome.textContent + teamAway.textContent
+  );
+  matches.splice(index, 1, match);
+  save(`matches-${group}`, matches);
+  renderMatches(group);
 }
