43940199b158e7a9cfdcf98bacea92b46413ff34
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2023-11-14 22:54:44 +0200

    update final page

--- final.js
+++ final.js
@@ -1,6 +1,5 @@
 const canv = document.getElementById("canvas");
 const ctx = canv.getContext("2d");
-
 dpi = window.devicePixelRatio;
 
 function fix_dpi() {
@@ -28,35 +27,45 @@ const load = (key) => {
 const stats = load("stats");
 const { team } = stats.sort((t1, t2) => t2.points - t1.points)[0];
 
-ctx.beginPath();
-ctx.fillStyle = "rgba(164, 61, 233, 0.7)";
-ctx.strokeStyle = "#fff";
-ctx.fillRect(30, 70, 200, 50);
-ctx.fillRect(30, 190, 200, 50);
-ctx.moveTo(230, 95);
-ctx.lineTo(270, 95);
-ctx.lineTo(270, 215);
-ctx.lineTo(230, 215);
-ctx.lineWidth = "5";
-ctx.stroke();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.fillStyle = "rgba(164, 61, 233, 0.7)";
+// ctx.strokeStyle = "#fff";
+// ctx.fillRect(30, 70, 200, 50);
+// ctx.fillRect(30, 190, 200, 50);
+// ctx.moveTo(230, 95);
+// ctx.lineTo(270, 95);
+// ctx.lineTo(270, 215);
+// ctx.lineTo(230, 215);
+// ctx.lineWidth = "5";
+// ctx.stroke();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.fillStyle = "#fff";
+// ctx.font = "35px Roboto";
+// ctx.fillText(`${team}`, 70, 105);
+// ctx.fillText(`${team}`, 70, 225);
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.fillStyle = "rgba(164, 61, 233, 0.7)";
+// ctx.strokeStyle = "#fff";
+// ctx.fillRect(260, 130, 200, 50);
+// ctx.fillRect(260, 350, 200, 50);
+// ctx.moveTo(460, 155);
+// ctx.lineTo(500, 155);
+// ctx.lineTo(500, 375);
+// ctx.lineTo(460, 375);
+// ctx.lineWidth = "5";
+// ctx.stroke();
+// ctx.closePath();
 
 ctx.beginPath();
-ctx.fillStyle = "#fff";
-ctx.font = "35px Roboto";
-ctx.fillText(`${team}`, 70, 105);
-ctx.fillText(`${team}`, 70, 225);
-ctx.closePath();
-
-ctx.beginPath();
-ctx.fillStyle = "rgba(164, 61, 233, 0.7)";
 ctx.strokeStyle = "#fff";
-ctx.fillRect(260, 130, 200, 50);
-ctx.fillRect(260, 350, 200, 50);
-ctx.moveTo(460, 155);
-ctx.lineTo(500, 155);
-ctx.lineTo(500, 375);
-ctx.lineTo(460, 375);
+ctx.moveTo(0, 30);
+ctx.lineTo(30, 30);
+ctx.lineTo(30, 155);
+ctx.lineTo(0, 155);
 ctx.lineWidth = "5";
 ctx.stroke();
 ctx.closePath();
